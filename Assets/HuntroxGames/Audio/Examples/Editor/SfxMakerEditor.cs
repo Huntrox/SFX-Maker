82e69b56e856ad4299991002f695d5d235eaa072
Huntrox <abde.had@hotmail.com>
2022-06-19 11:23:48 +0100

    Update SfxMakerEditor.cs

--- Assets/HuntroxGames/Audio/Examples/Editor/SfxMakerEditor.cs
+++ Assets/HuntroxGames/Audio/Examples/Editor/SfxMakerEditor.cs
@@ -7,6 +7,7 @@ namespace HuntroxGames
     public class SfxMakerEditor : Editor
     {
         private SfxMaker sfxMaker;
+
         private void OnEnable()
         {
             sfxMaker = (SfxMaker) target;
@@ -14,36 +15,33 @@ namespace HuntroxGames
 
         public override void OnInspectorGUI()
         {
-
-
             if (GUILayout.Button("Open Sfx Maker"))
                 SfxMakerUI.ShowExample();
             base.OnInspectorGUI();
             GUILayout.BeginHorizontal();
-            if(GUILayout.Button("Pickup"))
-                sfxMaker.Pickup();           
-            if(GUILayout.Button("Laser"))
-                sfxMaker.Laser();           
-            if(GUILayout.Button("Explosion"))
+            if (GUILayout.Button("Pickup"))
+                sfxMaker.Pickup();
+            if (GUILayout.Button("Laser"))
+                sfxMaker.Laser();
+            if (GUILayout.Button("Explosion"))
                 sfxMaker.Explosion();
             GUILayout.EndHorizontal();
-            
+
             GUILayout.BeginHorizontal();
-            if(GUILayout.Button("PowerUp"))
-                sfxMaker.PowerUp();           
-            if(GUILayout.Button("Hurt"))
-                sfxMaker.Hurt();           
-            if(GUILayout.Button("Jump"))
+            if (GUILayout.Button("PowerUp"))
+                sfxMaker.PowerUp();
+            if (GUILayout.Button("Hurt"))
+                sfxMaker.Hurt();
+            if (GUILayout.Button("Jump"))
                 sfxMaker.Jump();
             GUILayout.EndHorizontal();
-            
+
             GUILayout.BeginHorizontal();
-            if(GUILayout.Button("Blip"))
-                sfxMaker.Blip();           
-            if(GUILayout.Button("Play"))
+            if (GUILayout.Button("Blip"))
+                sfxMaker.Blip();
+            if (GUILayout.Button("Play"))
                 sfxMaker.Play();
             GUILayout.EndHorizontal();
-
         }
     }
 }
